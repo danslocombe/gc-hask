4e84e5c692f62a1db04c3bccc629178d3c91186f
Daniel Slocombe <ds4314@ic.ac.uk>
2018-01-12 06:23:19 +0000

    Formatting

--- src/PonyTypeSafeExamples.hs
+++ src/PonyTypeSafeExamples.hs
@@ -22,20 +22,6 @@ fieldsB :: Fields '[ '( 'Iso , 'Class "A" )
 fieldsB = (ObjectDescr2 1 1) `FieldCons` (Null2 `FieldCons` (Null2 `FieldCons` EmpFields))
 
 
-pathA :: Path2 0 '[0]
-pathA = SinglePath
-
-pathB :: Path2 0 '[1]
-pathB = SinglePath
-
-pathC :: Path2 0 '[0, 0]
-pathC = ChainPath SinglePath
-
-pathD :: Path2 0 '[2, 0]
-pathD = ChainPath SinglePath
-
-pathE :: Path2 0 '[1, 0]
-pathE = ChainPath SinglePath
 
 as0 = Actor2 1 fieldsB `ActStoreCons` EmpActStore
 os0 = Object2 1 1 fieldsA `ObjStoreCons` (Object2 1 2 EmpFields `ObjStoreCons` EmpObjStore)
@@ -62,6 +48,9 @@ cfg1 = modifyActFieldConfig (Proxy @0) (Proxy @0) ref1 cfg0
 -- Field 1 has Val capability so we can't write to it
 -- _ = modifyActFieldConfig (Proxy @0) (Proxy @1) Null2 cfg0
 
+pathA :: Path2 0 '[0]
+pathA = SinglePath
+
 ref2 :: Maybe (ObjectDescr2 ('Class "A"))
 ref2 = readPathConfig pathA cfg0 
 
@@ -69,13 +58,19 @@ ref2 = readPathConfig pathA cfg0
 -- ref2' :: Maybe (ObjectDescr2 ('Class "B"))
 -- ref2' = readPathConfig pathA cfg0 
 
+pathB :: Path2 0 '[0, 0]
+pathB = ChainPath SinglePath
+
 -- This is ok as Class A has a reference to another class A
-_ = readPathConfig pathC cfg0
+_ = readPathConfig pathB cfg0
 
 -- So this is too
-pathC' :: Path2 0 '[0, 0, 0, 0, 0]
-pathC' = ChainPath $ ChainPath $ ChainPath $ ChainPath SinglePath
-_ = readPathConfig pathC' cfg0
+pathB' :: Path2 0 '[0, 0, 0, 0, 0]
+pathB' = ChainPath $ ChainPath $ ChainPath $ ChainPath SinglePath
+_ = readPathConfig pathB' cfg0
+
+pathC :: Path2 0 '[2, 0]
+pathC = ChainPath SinglePath
 
 -- Class C doesnt have any fields so we cant do this
 -- _ = readPathConfig pathD cfg1
@@ -88,5 +83,8 @@ cfg2 = modifyPathConfig pathA Null2 cfg0
 -- ref3 = Null2
 -- cfg3 = modifyPathConfig pathA ref3 cfg0
 
+pathD :: Path2 0 '[1, 0]
+pathD = ChainPath SinglePath
+
 -- We can't write through a val reference
--- _ = modifyPathConfig pathE Null2 cfg0
+-- _ = modifyPathConfig pathD Null2 cfg0
